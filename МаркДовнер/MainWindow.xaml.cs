495a9fb043ba270501177a495f3116928013b598
kochkareal <kocharinovn@mail.ru>
2024-09-04 14:44:07 +0300

    Добавлен функционал - Сохранение
    *Теперь это просто обычный блокнот

--- МаркДовнер/MainWindow.xaml.cs
+++ МаркДовнер/MainWindow.xaml.cs
@@ -1,24 +1,32 @@
-﻿using System.Text;
+﻿// MainWindow.xaml.cs
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using МаркДовнер.ViewModels;
 
 namespace МаркДовнер
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Save, SaveExecuted));
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var viewModel = DataContext as MainViewModel;
+            viewModel?.SaveCommand.Execute(null);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.S && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                SaveExecuted(this, null);
+            }
+            base.OnKeyDown(e);
         }
     }
 }
---
+++ МаркДовнер/ViewModels/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿// ViewModels/MainViewModel.cs
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+
+namespace МаркДовнер.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _title = string.Empty;
+        private string _content = string.Empty;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                _content = value;
+                OnPropertyChanged(nameof(Content));
+            }
+        }
+
+        public ICommand SaveCommand { get; }
+
+        public MainViewModel()
+        {
+            Title = "Без названия";
+            SaveCommand = new RelayCommand(SaveFile);
+        }
+
+        private void SaveFile(object? obj) // Nullable параметр
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt|Markdown file (*.md)|*.md";
+            saveFileDialog.Title = "Сохранить файл как...";
+            saveFileDialog.FileName = GetValidFileName(Title);
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, Content);
+            }
+        }
+
+        private string GetValidFileName(string fileName)
+        {
+            var invalidChars = Path.GetInvalidFileNameChars();
+            return string.Concat(fileName.Where(c => !invalidChars.Contains(c)));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged; // Nullable событие
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
